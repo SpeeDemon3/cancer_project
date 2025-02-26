b2671736a1cc115c47a75c2d4204370dad256f08
Antonio Ruiz Benito <3speedemon3@gmail.com>
2025-02-26 07:18:29 +0100

    First commmit

---
+++ src/main/java/com/ruiz/cancer_project/CancerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ruiz.cancer_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancerProjectApplication.class, args);
+	}
+
+}
