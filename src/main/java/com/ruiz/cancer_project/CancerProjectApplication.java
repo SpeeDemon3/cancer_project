03b39f16dfef2400b452c6a44f813dff03376325
Antonio Ruiz Benito <3speedemon3@gmail.com>
2025-03-02 15:46:46 +0100

    First commit

--- src/main/java/com/ruiz/cancer_project/CancerProjectApplication.java
+++ src/main/java/com/ruiz/cancer_project/CancerProjectApplication.java
@@ -1,13 +1,18 @@
 package com.ruiz.cancer_project;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CancerProjectApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(CancerProjectApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CancerProjectApplication.class, args);
+		log.info("App is runing!!!!");
 	}
 
 }
